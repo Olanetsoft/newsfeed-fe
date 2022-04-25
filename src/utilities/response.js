c09113ff60154ae4ae269c2078143312bf0448e3
Idris Olubisi <heedris2olubisi@gmail.com>
2022-04-25 22:40:50 +0100

    Project Setup Complete

---
+++ src/utilities/response.js
@@ -0,0 +1,51 @@
+import { toast } from "react-toastify";
+
+const success = (message) => {
+  toast.success(message, {
+    position: "bottom-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+const error = (message) => {
+  toast.error(message, {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+const warn = (message) => {
+  toast.warn(message, {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+const defaultToast = (message) => {
+  toast(message, {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+export { success, error, warn, defaultToast };
